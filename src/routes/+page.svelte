1678935573dce6d1162e2a7c0dfaf03ec1f98741
Damiano Casula <d.casula@factor-eleven.de>
2023-11-25 11:02:17 +0100

    Page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,83 @@
 <script lang="ts">
-  let todos
+	import 'ress';
+
+	let todos = $state([
+		{
+			text: 'Learn Svelte',
+			done: false
+		},
+		{
+			text: 'Learn German',
+			done: true
+		}
+	]);
+
+	function addTodo(event: KeyboardEvent) {
+		if (event.key !== 'Enter') return;
+
+		const todoEl = event.target as HTMLInputElement;
+
+    // TODO: implement
+	}
 </script>
+
+<div class="page">
+	<div class="content">
+		<input type="text" placeholder="Add a todo" onkeydown={addTodo} />
+
+		<div class="todos">
+			{#each todos as todo, i}
+				<div class="todo">
+					<input type="text" value={todo.text} />
+					<input type="checkbox" checked={todo.done} />
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	.page {
+		background-color: #fafafa;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.content {
+		padding: 1rem;
+		border-radius: 1rem;
+		background-color: #eee;
+		box-shadow:
+			0.5rem 0.5rem 1rem #ccc,
+			-0.5rem -0.5rem 1rem #ddd;
+		max-width: 800px;
+		border: 1px solid #bbb;
+	}
+
+	.todos {
+		display: grid;
+		gap: 1rem;
+		margin-block-start: 1rem;
+	}
+
+	.todo {
+		position: relative;
+		background-color: #eee;
+		border-radius: 0.5rem;
+		border: 1px solid #ddd;
+	}
+
+	input[type='text'] {
+		width: 100%;
+		padding: 1rem;
+	}
+
+	input[type='checkbox'] {
+		position: absolute;
+		right: 4%;
+		top: 50%;
+		translate: 0 -50%;
+	}
+</style>
